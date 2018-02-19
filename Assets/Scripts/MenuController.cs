81ec4e4a7312485f608c759a6db47e86b5126d26
Whitney Tran <whitnet@uci.edu>
2018-02-18 19:22:47 -0800

    Finished StartMenu and Created PauseMenu

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -5,8 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour {
 
+    public void StartScene()
+    {
+        SceneManager.LoadScene("2_game");
+    }
+
 	public void RestartScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
